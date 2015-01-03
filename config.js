f9b098c53f44391df0f569a675bac2761ac36331
Razzi Abuissa <razzi53@gmail.com>
2015-01-03 16:51:00 -0600

    Configure for Heroku deployment

--- config.js
+++ config.js
@@ -6,25 +6,28 @@ var path = require('path'),
     config;
 
 config = {
-    // ### Production
-    // When running Ghost in the wild, use the production environment
-    // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
-        mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
+        url: 'http://bigwetrecords.com',
+        mail: {
+            from: 'Big Wet Records <bigwetrecords@gmail.com>',
+            transport: 'SMTP',
+            options: {
+                service: 'Gmail',
+                auth: {
+                    user: 'bigwetrecords@gmail.com',
+                    pass: process.env.GMAIL_PASSWORD
+                }
+            }
         },
-
+        database: {
+            client: 'postgres',
+            connection: process.env.DATABASE_URL + '?ssl=true',
+        },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: process.env.PORT
         }
     },
 
@@ -57,17 +60,13 @@ config = {
             debug: false
         },
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: process.env.PORT || 2368
         },
         paths: {
             contentPath: path.join(__dirname, '/content/')
         }
     },
-
-    // **Developers only need to edit below here**
 
     // ### Testing
     // Used when developing Ghost to run tests and check the health of Ghost
@@ -78,48 +77,6 @@ config = {
             client: 'sqlite3',
             connection: {
                 filename: path.join(__dirname, '/content/data/ghost-test.db')
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Testing MySQL
-    // Used by Travis - Automated testing run through GitHub
-    'testing-mysql': {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'mysql',
-            connection: {
-                host     : '127.0.0.1',
-                user     : 'root',
-                password : '',
-                database : 'ghost_testing',
-                charset  : 'utf8'
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Testing pg
-    // Used by Travis - Automated testing run through GitHub
-    'testing-pg': {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'pg',
-            connection: {
-                host     : '127.0.0.1',
-                user     : 'postgres',
-                password : '',
-                database : 'ghost_testing',
-                charset  : 'utf8'
             }
         },
         server: {
